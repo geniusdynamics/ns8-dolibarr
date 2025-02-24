90c6cd91261cb455713069f8c703a9a2c2a13839
Martin Otieno <martin@genius.ke>
2025-02-24 10:55:40 +0300

    upgrade dolibar to version 20.0.3

--- build-images.sh
+++ build-images.sh
@@ -14,7 +14,7 @@ images=()
 repobase="${REPOBASE:-ghcr.io/geniusdynamics}"
 # Configure the image name
 reponame="dolibarr"
-DOLIBARR_TAG="19.0.2"
+DOLIBARR_TAG="20.0.3"
 # Create a new empty container image
 container=$(buildah from scratch)
 
